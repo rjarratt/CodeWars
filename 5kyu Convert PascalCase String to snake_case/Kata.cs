e1358195e5ae5869a806805666635f757d327d0e
Robert Jarratt <3358632+rjarratt@users.noreply.github.com>
2023-08-27 17:49:32 +0100

    Snake case Kata

---
+++ 5kyu Convert PascalCase String to snake_case/Kata.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+using System.Text;
+
+public static class Kata
+{
+    public static string ToUnderscore(int str)
+    {
+        return str.ToString(CultureInfo.InvariantCulture);
+    }
+
+    public static string ToUnderscore(string str)
+    {
+        StringBuilder snakeCase = new StringBuilder();
+        foreach (char c in str)
+        {
+            if (char.IsUpper(c))
+            {
+                if (snakeCase.Length > 0)
+                {
+                    snakeCase.Append('_');
+                }
+
+                snakeCase.Append(char.ToLowerInvariant(c));
+            }
+            else
+            {
+                snakeCase.Append(c);
+            }
+        }
+
+        return snakeCase.ToString();
+    }
+}
---
+++ 5kyu Convert PascalCase String to snake_case/Tests.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[TestClass]
+public class Tests
+{
+    [TestMethod]
+    [DataRow("TestController", "test_controller")]
+    [DataRow("ThisIsBeautifulDay", "this_is_beautiful_day")]
+    [DataRow("Am7Days", "am7_days")]
+    [DataRow("My3CodeIs4TimesBetter", "my3_code_is4_times_better")]
+    [DataRow("ArbitrarilySendingDifferentTypesToFunctionsMakesKatasCool", "arbitrarily_sending_different_types_to_functions_makes_katas_cool")]
+    public void SampleStringTests(string value, string expectedResult)
+    {
+        Kata.ToUnderscore(value).Should().Be(expectedResult);
+    }
+
+    [TestMethod]
+    [DataRow(1, "1")]
+    public void SampleIntTests(int value, string expectedResult)
+    {
+        Kata.ToUnderscore(value).Should().Be(expectedResult);
+    }
+}
