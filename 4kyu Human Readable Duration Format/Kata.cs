ab2aa2cf0606b68b64d4c5cac945bc31a0680fcb
Robert Jarratt <3358632+rjarratt@users.noreply.github.com>
2023-08-27 22:22:23 +0100

    Human readable duration format kata

---
+++ 4kyu Human Readable Duration Format/Kata.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+public class HumanTimeFormat
+{
+    private static PeriodDescription[] periodDescriptions =
+    {
+        new PeriodDescription { Description = "second", UnitsInNextPeriod = 60 },
+        new PeriodDescription { Description = "minute", UnitsInNextPeriod = 60 },
+        new PeriodDescription { Description = "hour", UnitsInNextPeriod = 24 },
+        new PeriodDescription { Description = "day", UnitsInNextPeriod = 365 },
+        new PeriodDescription { Description = "year", UnitsInNextPeriod = int.MaxValue },
+    };
+
+    public static string formatDuration(int seconds)
+    {
+        StringBuilder result = new StringBuilder();
+
+        if (seconds <= 0)
+        {
+            result.Append("now");
+        }
+        else
+        { 
+            List<string> part = new List<string>();
+            int period = seconds;
+            for (int i = 0; i < periodDescriptions.Length && period > 0; i++)
+            {
+                PeriodDescription periodDescription = periodDescriptions[i];
+                int duration = period % periodDescription.UnitsInNextPeriod;
+                period = period / periodDescription.UnitsInNextPeriod;
+                if (duration > 1)
+                {
+                    part.Insert(0, $"{duration} {periodDescription.Description}s");
+                }
+                else if (duration > 0)
+                {
+                    part.Insert(0, $"1 {periodDescription.Description}");
+                }
+            }
+
+            for (int i = 0; i < part.Count; i++)
+            {
+                result.Append(part[i]);
+                if (i < part.Count - 2)
+                {
+                    result.Append(", ");
+                }
+                else if (i < part.Count - 1)
+                {
+                    result.Append(" and ");
+                }
+            }
+        }
+
+        return result.ToString();
+    }
+
+    private struct PeriodDescription
+    {
+        public string Description { get; set; }
+
+        public int UnitsInNextPeriod { get; set; }
+    }
+}
---
+++ 4kyu Human Readable Duration Format/Tests.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[TestClass]
+public class Tests
+{
+    [TestMethod]
+    [DataRow(0, "now")]
+    [DataRow(1, "1 second")]
+    [DataRow(62, "1 minute and 2 seconds")]
+    [DataRow(120, "2 minutes")]
+    [DataRow(3662, "1 hour, 1 minute and 2 seconds")]
+    [DataRow(15731080, "182 days, 1 hour, 44 minutes and 40 seconds")]
+    [DataRow(132030240, "4 years, 68 days, 3 hours and 4 minutes")]
+    [DataRow(205851834, "6 years, 192 days, 13 hours, 3 minutes and 54 seconds")]
+    [DataRow(253374061, "8 years, 12 days, 13 hours, 41 minutes and 1 second")]
+    [DataRow(242062374, "7 years, 246 days, 15 hours, 32 minutes and 54 seconds")]
+    [DataRow(101956166, "3 years, 85 days, 1 hour, 9 minutes and 26 seconds")]
+    [DataRow(33243586, "1 year, 19 days, 18 hours, 19 minutes and 46 seconds")]
+    public void BasicTests(int seconds, string expected)
+    {
+        HumanTimeFormat.formatDuration(seconds).Should().Be(expected);
+    }
+}
