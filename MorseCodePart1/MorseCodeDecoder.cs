648c95d0ad8c2e24234bd94502a4452e7a45dc35
Robert Jarratt <3358632+rjarratt@users.noreply.github.com>
2023-09-01 22:33:15 +0100

    Morse Code Part 1

---
+++ MorseCodePart1/MorseCodeDecoder.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+
+public class MorseCodeDecoder
+{
+    public static string Decode(string morseCode)
+    {
+        string[] words = morseCode.Trim().Split("   ");
+        string result = string.Join(' ', words.Select(word => DecodeWord(word)));
+        return result;
+    }
+
+    private static string DecodeWord(string word)
+    {
+        string result = string.Concat(word.Split(' ').Select(letter => MorseCode.Get(letter)));
+        return result;
+    }
+}
---
+++ MorseCodePart1/Tests.cs
@@ -0,0 +1,16 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[TestClass]
+public class Tests
+{
+    [TestMethod]
+    [DataRow(".... . -.--   .--- ..- -.. .", "HEY JUDE")]
+    [DataRow("  .... . -.--   .--- ..- -.. .  ", "HEY JUDE")]
+    public void MorseCodeDecoderBasicTest(string input, string expected)
+    {
+        string actual = MorseCodeDecoder.Decode(input);
+
+        expected.Should().Be(actual);
+    }
+}
