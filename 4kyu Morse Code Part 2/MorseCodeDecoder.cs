d6fa8d45ca83b512615bbeca6316495ebc9bb13d
Rob <rob:@jarratt.me.uk>
2023-09-03 07:24:09 +0100

    Solution strucure with part 1 decoder included.

---
+++ 4kyu Morse Code Part 2/MorseCodeDecoder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+public class MorseCodeDecoder
+{
+    public static string DecodeBits(string bits)
+    {
+        return ".";
+    }
+
+    public static string DecodeMorse(string morseCode)
+    {
+        string[] words = morseCode.Trim().Split("   ");
+        string result = string.Join(' ', words.Select(word => DecodeWord(word)));
+        return result;
+    }
+
+    private static string DecodeWord(string word)
+    {
+        string result = string.Concat(word.Split(' ').Select(letter => MorseCode.Get(letter)));
+        return result;
+    }
+}
---
+++ 4kyu Morse Code Part 2/Tests.cs
@@ -0,0 +1,16 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MorseCodePart2;
+
+[TestClass]
+public class Tests
+{
+    [TestMethod]
+    [DataRow("1100110011001100000011000000111111001100111111001111110000000000000011001111110011111100111111000000110011001111110000001111110011001100000011", "HEY JUDE")]
+    public void TestExampleFromDescription(string bits, string expected)
+    {
+        MorseCodeDecoder.DecodeMorse(MorseCodeDecoder.DecodeBits(bits)).Should().Be(expected);
+
+    }
+}
