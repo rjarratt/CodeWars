d01349a4f20cf700766b2f8e6af7adf8281402e7
Rob <rob:@jarratt.me.uk>
2023-09-03 09:18:36 +0100

    Morse code part 2 solution

--- 4kyu Morse Code Part 2/MorseCodeDecoder.cs
+++ 4kyu Morse Code Part 2/MorseCodeDecoder.cs
@@ -1,15 +1,66 @@
 ﻿using System;
+using System.Linq;
+
 public class MorseCodeDecoder
 {
     public static string DecodeBits(string bits)
     {
-        return ".";
+        // In this version it seems we can assume that the time periods are all the same.
+        // To determine how many bits per time period we just look for the shortest run of 1s.
+
+        string trimmedBits = bits.Trim('0');
+        int bitsPerPeriod = ComputeBitPeriod(trimmedBits);
+        string dot = new string('1', bitsPerPeriod);
+        string dash = new string('1', 3 * bitsPerPeriod);
+        string dotDashPause = new string('0', bitsPerPeriod);
+        string characterSeparator = new string('0', 3 * bitsPerPeriod);
+        string wordSeparator = new string('0', 7 * bitsPerPeriod);
+
+        string result = trimmedBits
+            .Replace(wordSeparator, "   ")
+            .Replace(characterSeparator, " ")
+            .Replace(dash, "-")
+            .Replace(dot, ".")
+            .Replace(dotDashPause, "");
+
+        return result;
     }
 
     public static string DecodeMorse(string morseCode)
     {
         string[] words = morseCode.Trim().Split("   ");
         string result = string.Join(' ', words.Select(word => DecodeWord(word)));
+        return result;
+    }
+
+    private static int ComputeBitPeriod(string bits)
+    {
+        int result = int.MaxValue;
+        int currentRun = 0;
+        char currentRunChar = ' ';
+        foreach(char bit in bits)
+        {
+            if (bit != currentRunChar)
+            {
+                if (currentRun < result && currentRun > 0)
+                {
+                    result = currentRun;
+                }
+
+                currentRunChar = bit;
+                currentRun = 1;
+            }
+            else
+            {
+                currentRun++;
+            }
+        }
+
+        if (currentRun < result && currentRun > 0)
+        {
+            result = currentRun;
+        }
+
         return result;
     }
 
--- 4kyu Morse Code Part 2/Tests.cs
+++ 4kyu Morse Code Part 2/Tests.cs
@@ -3,12 +3,22 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace MorseCodePart2;
 
+// https://www.codewars.com/kata/54b72c16cd7f5154e9000457/train/csharp
 [TestClass]
 public class Tests
 {
     [TestMethod]
     [DataRow("1100110011001100000011000000111111001100111111001111110000000000000011001111110011111100111111000000110011001111110000001111110011001100000011", "HEY JUDE")]
     public void TestExampleFromDescription(string bits, string expected)
+    {
+        MorseCodeDecoder.DecodeMorse(MorseCodeDecoder.DecodeBits(bits)).Should().Be(expected);
+
+    }
+
+    [TestMethod]
+    [DataRow("1", "E")]
+    [DataRow("1110111", "M")]
+    public void OtherExamplesDiscernedFromFailedAttempts(string bits, string expected)
     {
         MorseCodeDecoder.DecodeMorse(MorseCodeDecoder.DecodeBits(bits)).Should().Be(expected);
 
