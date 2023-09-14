1413b67902890c6d287e9bba9531d784137e634d
Robert Jarratt <3358632+rjarratt@users.noreply.github.com>
2023-09-14 21:52:04 +0100

    Structure with initial tests, all failing

---
+++ 2kyu Mathematical Expressions/Evaluate.cs
@@ -0,0 +1,12 @@
+﻿namespace Evaluation;
+
+using System;
+
+public class Evaluate
+{
+    public string eval(string expression)
+    {
+        string result = "0"; //calculated expression (double converted to string) or Errormessage starting with "ERROR" (+ optional Errormessage)
+        return result;
+    }
+}
---
+++ 2kyu Mathematical Expressions/Tests.cs
@@ -0,0 +1,22 @@
+﻿namespace Evaluation;
+
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[TestClass]
+public class AccountTest
+{
+    private readonly Evaluate ev = new Evaluate();
+
+    [TestMethod]
+    [DataRow("18", "2*3&2")]
+    [DataRow("-240", "(-(2 + 3)* (1 + 2)) * 4 & 2")]
+    [DataRow("ERROR", "sqrt(-2)*2")]
+    [DataRow("ERROR", "2*5/0")]
+    [DataRow("-3906251", "-5&3&2*2-1")]
+    [DataRow("169", "abs(-(-1+(2*(4--3)))&2)")]
+    public void TestCases(string expected, string expression)
+    {
+        this.ev.eval(expression).Should().Be(expected);
+    }
+}
