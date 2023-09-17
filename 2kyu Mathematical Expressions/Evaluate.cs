5028eec26755b1e317e04af6e3fad7e9cded1839
Robert Jarratt <3358632+rjarratt@users.noreply.github.com>
2023-09-17 07:14:36 +0100

    More than one function call

--- 2kyu Mathematical Expressions/Evaluate.cs
+++ 2kyu Mathematical Expressions/Evaluate.cs
@@ -129,7 +129,6 @@ public class Evaluate
                     }
 
                     moreTokens = tokenEnumerator.MoveNext();
-                    endOfExpression = true;
 
                     switch (function)
                     {
