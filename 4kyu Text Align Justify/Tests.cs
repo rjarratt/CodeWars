3075444835fbbe178385e30ac04d91425697125a
Rob Jarratt <rjarratt@microsoft.com>
2023-11-01 08:18:02 +0000

    Naive implementation

--- 4kyu Text Align Justify/Tests.cs
+++ 4kyu Text Align Justify/Tests.cs
@@ -8,6 +8,7 @@ public class SolutionTest
 {
     [TestMethod]
     [DataRow("123 45 6", 7, "123  45\n6")]
+    [DataRow("123456", 6, "123456")]
     public void MyTest(string input, int lineLength, string expectedOutput)
     {
         Kata.Justify(input, lineLength).Should().Be(expectedOutput);
