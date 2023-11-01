b1f2c9768ebef18b69bc15cd562f74ac736b1dae
Rob Jarratt <rjarratt@microsoft.com>
2023-11-01 07:47:40 +0000

    Text Align Justify kata solution

---
+++ 4kyu Text Align Justify/Kata.cs
@@ -0,0 +1,9 @@
+﻿namespace Solution;
+
+public class Kata
+{
+    public static string Justify(string str, int len)
+    {
+        return "";
+    }
+}
---
+++ 4kyu Text Align Justify/Tests.cs
@@ -0,0 +1,15 @@
+﻿namespace Solution;
+
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[TestClass]
+public class SolutionTest
+{
+    [TestMethod]
+    [DataRow("123 45 6", 7, "123  45\n6")]
+    public void MyTest(string input, int lineLength, string expectedOutput)
+    {
+        Kata.Justify(input, lineLength).Should().Be(expectedOutput);
+    }
+}
